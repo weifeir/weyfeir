5b0e1fdf7fe9a41a8abe271fad33bd0c7404ab19
weifeir <weyfeir@gmail.com>
2025-07-01 13:54:06 +0500

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,4 @@
-        <Avatar 
+<Avatar 
           sx={{ ml: 1, width: 36, height: 36 }}
-          alt={senderName === "mdziq962#@gmail.com" || senderName === "Customer Care" ? "Customer Care" : displayName}
-        /> 
+          alt={senderName === "weyfeiradmin@gmail.com" || senderName === "Customer Care" ? "Customer Care" : displayName}
+        />
